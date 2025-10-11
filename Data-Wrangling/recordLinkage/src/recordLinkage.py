7002100681018facecc34ed2b519f4c74f8e4168
Razeen Wasif <razeen.wasif66@gmail.com>
2025-10-11 14:09:33 +1100

    BANZAI

--- Data-Wrangling/recordLinkage/src/recordLinkage.py
+++ Data-Wrangling/recordLinkage/src/recordLinkage.py
@@ -207,22 +207,22 @@ def main():
             )
 
         elif dataset_category == 'very_dirty':
-            first_loose = first_col >= 0.60
-            last_loose = last_col >= 0.70
-            postcode_relaxed = postcode_col >= 0.88
-            suburb_relaxed = suburb_col >= 0.80
-            address_relaxed = address_col >= 0.75
-            phone_relaxed = phone_col >= 0.78
-            birth_relaxed = birth_col >= 0.90
-            gender_relaxed = gender_col >= 0.85
+            first_loose = first_col >= 0.65
+            last_loose = last_col >= 0.75
+            postcode_relaxed = postcode_col >= 0.90
+            suburb_relaxed = suburb_col >= 0.85
+            address_relaxed = address_col >= 0.78
+            phone_relaxed = phone_col >= 0.80
+            birth_relaxed = birth_col >= 0.93
+            gender_relaxed = gender_col >= 0.90
 
             location_core = postcode_relaxed & suburb_relaxed
 
             keep_mask = (
-                (location_core & (last_loose | address_relaxed | phone_relaxed | birth_relaxed | first_loose | gender_relaxed))
-                | (address_relaxed & suburb_relaxed & (last_loose | first_loose | phone_relaxed | birth_relaxed))
-                | (phone_relaxed & (last_loose | first_loose) & (location_core | address_relaxed | gender_relaxed))
-                | (birth_relaxed & (last_loose | first_loose | gender_relaxed))
+                (location_core & (address_relaxed | phone_relaxed | birth_relaxed | (first_loose & last_loose)))
+                | (address_relaxed & suburb_relaxed & (phone_relaxed | birth_relaxed | last_loose))
+                | (phone_relaxed & last_loose & (first_loose | gender_relaxed | location_core))
+                | (birth_relaxed & last_loose & (first_loose | gender_relaxed))
                 | ((first_loose & last_loose) & (location_core | address_relaxed | phone_relaxed | birth_relaxed))
             )
         else:
